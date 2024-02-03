2fa17c44c0927bc5009d2c4522306c8254bc037a
паша <@LAPTOP-ROBAKT4L>
2024-02-03 21:34:44 +0200

    finish

--- Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
+++ Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
@@ -8,7 +8,7 @@ enum Root_Types {
       Equal_roots,
       Two_roots,
       ERROR
-      
+      //
 	}
 
 //Roots class source from example
@@ -28,7 +28,7 @@ class Roots
       
       q = b*b - 4*a*c;
      
-      if (out_of_Range(a, 0, 50)|| out_of_Range(b, 0, 50)||out_of_Range(c, 0, 100)) {
+      if (out_of_Range(a, 0, 100)|| out_of_Range(b, 0, 100)||out_of_Range(c, 0, 100)) {
     	  roots = Root_Types.ERROR;
     	  return;
       }
@@ -36,33 +36,25 @@ class Roots
       if (a != 0) {
 	      if(q>0 )
 	      {
-		      roots = Root_Types.Equal_roots; 
+		      roots = Root_Types.Two_roots; 
 	      }
 	      else if(q==0)
 	      {
 		      // the equation has exactly one root
 		     
 		     
-		      roots = Root_Types.Two_roots; 
+		      roots = Root_Types.Equal_roots; 
 		      
 	      } 
 	      else
 	      {
 		      // the equation has no roots if b^2 < 4ac
 		     
-		      roots = Root_Types.Not_quadratic;
+		      roots = Root_Types.No_roots;
 	      }
       }
       else {
     	  
-    	  roots = Root_Types.Two_roots; 
+    	  roots = Root_Types.Not_quadratic; 
       }
    }
-
-    public static Root_Types root_types()
-    {
-    	
-    	return roots;
-    }
-
- }
